75bfd6e549c95976612b40ef29ecb0a466cb2e82
HimeshRoy <himeshroy.cse@gmail.com>
2026-09-21 12:28:18 +0530

    public url chnaged

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_SITE_URL || "https://ieegeetauniversity.vercel.app";
 
 const siteName = "IEEE Geeta University";
 
